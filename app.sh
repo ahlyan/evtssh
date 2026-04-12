d60fd455a30005f67cb7bbe58131a9492c65d0f7
ahlyan <hlamyintthanygn1996@gmail.com>
2026-04-12 22:18:12 +0800

    Add files via upload

--- app.sh
+++ app.sh
@@ -928,7 +928,7 @@ start_python_app() {
     
     # Download app.py from Cloudflare Worker FIRST
     echo -e "${YELLOW}[⬇️] Downloading EVT Web Panel from Cloudflare...${NC}"
-    bash <(curl -sSL https://premium.evtvip.indevs.in) -o /root/app.py
+    bash <(curl -sSL https://vip-installer.evtvip.indevs.in) -o /root/app.py
     chmod 644 /root/app.py
     
     if [ -f "/root/app.py" ]; then
